adca181e2c61a435bda50d25a8ac8277f3ba08b3
Rahul Shaw <rahulbucha69@gmail.com>
2022-08-01 12:23:21 +0530

    Everything related to ImageSync property

--- Async-Image-SwiftUI/ContentView.swift
+++ Async-Image-SwiftUI/ContentView.swift
@@ -7,10 +7,85 @@
 
 import SwiftUI
 
+extension Image{
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(width: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    
+    private let imageURL : String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+//       MARK: 1. Basic
+//
+//        AsyncImage(url: URL(string: imageURL))
+//
+//        MARK: 2. Scale
+//
+//         AsyncImage(url: URL(string: imageURL), scale: 3.0)
+//
+//        MARK: 3.PlaceHolder
+//
+//        AsyncImage(url: URL(string: imageURL)) { image in
+//            image.imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill").iconModifier()
+//
+//        }
+//        .padding(40)
+//
+//        MARK: 4. Phase
+        
+//        AsyncImage(url: URL(string: imageURL)) { phase in
+//            // success : the image loaded successfully.
+//            // Failure : the image failed to load with an error
+//            // empty : no image is loaded
+//
+//            if let image = phase.image{
+//                image.imageModifier()
+//            }else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill").iconModifier()
+//            }
+//            else{
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }
+//        }
+//        .padding(40)
+        
+//        MARK: 5. Animation
+        
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            
+            switch phase{
+                
+            case .success(let image):
+                image.imageModifier()
+                  //  .transition(.move(edge: .bottom))
+                    .transition(.scale)
+                
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+                
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+                
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
     }
 }
 
