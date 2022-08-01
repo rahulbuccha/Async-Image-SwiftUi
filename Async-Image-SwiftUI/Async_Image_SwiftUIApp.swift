6ab5e33f958be8bd97640d8248a69a777800c30a
Rahul Shaw <rahulbucha69@gmail.com>
2022-08-01 11:36:08 +0530

    Initial Commit

---
+++ Async-Image-SwiftUI/Async_Image_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Image_SwiftUIApp.swift
+//  Async-Image-SwiftUI
+//
+//  Created by Admin on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_Image_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
